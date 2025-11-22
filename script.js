427a117a6b3b77e35f09f4a1790957e80e67803d
zackkwendo <84226638+zackkwendo@users.noreply.github.com>
2025-11-22 08:18:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+console.log("JavaScript is working!");
+let text = "Hello, I'm Kwendo Mboya";
+let index = 0;
+
+function typeEffect() {
+    let typing = document.getElementById("typing");
+
+    if (index < text.length) {
+        typing.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+typeEffect();
+let toTop = document.getElementById("toTop");
+
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        toTop.style.display = "block";
+    } else {
+        toTop.style.display = "none";
+    }
+};
+
+toTop.onclick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("shrink");
+    } else {
+        header.classList.remove("shrink");
+    }
+});
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
+
+// Load theme from memory
+if (localStorage.getItem("theme") === "light") {
+    body.classList.add("light-mode");
+    toggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+}
+
+// Toggle button
+toggle.addEventListener("click", () => {
+    body.classList.toggle("light-mode");
+
+    if (body.classList.contains("light-mode")) {
+        toggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        localStorage.setItem("theme", "light");
+    } else {
+        toggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        localStorage.setItem("theme", "dark");
+    }
+});
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+document.querySelector(".next").addEventListener("click", () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+});
+
+document.querySelector(".prev").addEventListener("click", () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+});
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}, 4000);
